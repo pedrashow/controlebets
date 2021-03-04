f43174596bfc645cd99ff98c6f831dea03a44759
pedrashow <roberto.oliveira@uniriotec.br>
2021-03-04 12:16:18 -0300

    bet atualizada

--- src/model/Bet.js
+++ src/model/Bet.js
@@ -55,7 +55,10 @@ const BetSchema = new mongoose.Schema({
 		type: mongoose.Types.Decimal128,
 		required: true,
 	},
-	bettorResult: {
+	betResult: {
+		type: mongoose.Types.Decimal128,
+	},
+	betResultInEur: {
 		type: mongoose.Types.Decimal128,
 	}
 });
@@ -66,40 +69,40 @@ BetSchema.methods.calcResult = function (homeScore, awayScore) {
 	const totalScore = parseInt(homeScore) + parseInt(awayScore);
 	switch (this.selection) {
 		case 'home':
-			this.bettorResult = this.winner(scoreDiff, winValue);
+			this.betResult = this.winner(scoreDiff, winValue);
 			break;
 		case 'away':
-			this.bettorResult = this.winner(-1 * scoreDiff, winValue);
+			this.betResult = this.winner(-1 * scoreDiff, winValue);
 			break;
 		case 'draw':
-			this.bettorResult = this.draw(scoreDiff, winValue);
+			this.betResult = this.draw(scoreDiff, winValue);
 			break;
 		case 'asianHome':
-			this.bettorResult = this.asianCalc(scoreDiff, this.selectionHandicap, winValue);
+			this.betResult = this.asianCalc(scoreDiff, this.selectionHandicap, winValue);
 			break;
 		case 'asianAway':
-			this.bettorResult = this.asianCalc(-1 * scoreDiff, this.selectionHandicap, winValue);
+			this.betResult = this.asianCalc(-1 * scoreDiff, this.selectionHandicap, winValue);
 			break;
 		case 'under':
-			this.bettorResult = this.asianCalc(-1 * totalScore, this.selectionHandicap, winValue)
+			this.betResult = this.asianCalc(-1 * totalScore, this.selectionHandicap, winValue)
 			break;
 		case 'over':
-			this.bettorResult = this.asianCalc(totalScore, -1 * this.selectionHandicap, winValue);
+			this.betResult = this.asianCalc(totalScore, -1 * this.selectionHandicap, winValue);
 			break;
 		case 'homeOver':
-			this.bettorResult = this.asianCalc(homeScore, -1 * this.selectionHandicap, winValue);
+			this.betResult = this.asianCalc(homeScore, -1 * this.selectionHandicap, winValue);
 			break;
 		case 'homeUnder':
-			this.bettorResult = this.asianCalc(-1 * homeScore, this.selectionHandicap, winValue);
+			this.betResult = this.asianCalc(-1 * homeScore, this.selectionHandicap, winValue);
 			break;
 		case 'awayOver':
-			this.bettorResult = this.asianCalc(awayScore, -1 * this.selectionHandicap, winValue);
+			this.betResult = this.asianCalc(awayScore, -1 * this.selectionHandicap, winValue);
 			break;
 		case 'awayUnder':
-			this.bettorResult = this.asianCalc(-1 * awayScore, this.selectionHandicap, winValue);
+			this.betResult = this.asianCalc(-1 * awayScore, this.selectionHandicap, winValue);
 			break;
 		case 'btts':
-			this.bettorResult = this.btts(homeScore, awayScore, winValue);
+			this.betResult = this.btts(homeScore, awayScore, winValue);
 			break;
 	}
 };
@@ -138,18 +141,22 @@ BetSchema.methods.btts = function (home, away, winValue) {
 
 BetSchema.methods.convertToEur = async function(value, currencyId) {
 	const currency = await Currency.findById(currencyId);
-	this.stakeInEur = parseFloat(parseFloat(value,2) * parseFloat(currency.rateToEur,2), 2);
+	return parseFloat(parseFloat(value,2) * parseFloat(currency.rateToEur,2), 2);
 }
 
 BetSchema.pre('validate', async function(next) {
 	if (!this.stakeInEur)
-		await this.convertToEur(this.stake, this.stakeCurrency);
+		this.stakeInEur = await this.convertToEur(this.stake, this.stakeCurrency);
 	next();
 });
 
 BetSchema.pre('save', async function(next) {
-	if (this.bettorResult)
-		this.bettorResult = parseFloat(this.bettorResult, 2);	
+	if (this.betResult) {
+		this.betResult = parseFloat(this.betResult, 2);
+		this.betResultInEur = parseFloat(await this.convertToEur(this.betResult, this.stakeCurrency),2);
+	}
+	next();
+		
 })
 
 
